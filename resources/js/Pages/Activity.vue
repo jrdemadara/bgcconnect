e93ead9df5e2e920a91b76740a188aee16366d11
Johnny Roger <jrdemadara@protonmail.com>
2024-09-16 16:19:36 +0800

    ini: added verification logs

--- resources/js/Pages/Activity.vue
+++ resources/js/Pages/Activity.vue
@@ -1,12 +1,74 @@
-<script setup lang="ts">
-import ref from "vue";
+<script setup>
+import { ref, computed, createApp } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
 
-defineProps<{
-    isPhoneVerified?: boolean;
-    status?: string;
-}>();
+// defineProps<{
+//     isPhoneVerified?: boolean;
+//     status?: string;
+// }>();
+
+const result = ref("");
+const error = ref("");
+const showScanConfirmation = ref(false);
+
+function paintBoundingBox(detectedCodes, ctx) {
+    for (const detectedCode of detectedCodes) {
+        const {
+            boundingBox: { x, y, width, height },
+        } = detectedCode;
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#007bff";
+        ctx.strokeRect(x, y, width, height);
+    }
+}
+
+function onError(err) {
+    console.log(err);
+    error.value = `[${err.name}]: `;
+
+    if (err.name === "NotAllowedError") {
+        error.value += "you need to grant camera access permission";
+    } else if (err.name === "NotFoundError") {
+        error.value += "no camera on this device";
+    } else if (err.name === "NotSupportedError") {
+        error.value += "secure context required (HTTPS, localhost)";
+    } else if (err.name === "NotReadableError") {
+        error.value += "is the camera already in use?";
+    } else if (err.name === "OverconstrainedError") {
+        error.value += "installed cameras are not suitable";
+    } else if (err.name === "StreamApiNotSupportedError") {
+        error.value += "Stream API is not supported in this browser";
+    } else if (err.name === "InsecureContextError") {
+        error.value +=
+            "Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
+    } else {
+        error.value += err.message;
+    }
+}
+
+function onCameraOn(detectedCodes) {
+    showScanConfirmation.value = false;
+}
+
+function onCameraOff(detectedCodes) {
+    showScanConfirmation.value = true;
+}
+
+function onDetect(detectedCodes) {
+    let code = detectedCodes.map((code) => code.rawValue);
+    result.value = code[0];
+}
+
+const app = createApp({
+    setup() {
+        return { result, error, onDetect, onError, paintBoundingBox };
+    },
+});
+app.use(QrcodeStream);
+app.mount("#app");
 </script>
 <template>
     <Head title="Profile" />
@@ -25,12 +87,26 @@ defineProps<{
                 <p class="font-bold text-lg my-4">
                     Point the camera to the QR Code
                 </p>
-                <img
-                    class="w-full h-32 bg-blue-100 rounded-lg"
-                    src=""
-                    alt=""
-                    srcset=""
-                />
+                <p style="color: red">{{ error }}</p>
+
+                <p>
+                    Last result: <b>{{ result }}</b>
+                </p>
+
+                <div class="border-4 border-gray-500">
+                    <qrcode-stream
+                        :track="paintBoundingBox"
+                        @detect="onDetect"
+                        @camera-on="onCameraOn"
+                        @camera-off="onCameraOff"
+                        @error="onError"
+                    >
+                        <div
+                            v-show="showScanConfirmation"
+                            class="scan-confirmation"
+                        ></div
+                    ></qrcode-stream>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>
