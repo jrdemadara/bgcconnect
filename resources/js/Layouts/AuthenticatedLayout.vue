7679da989c13fe5ef5e4724e4099427afc94c178
jrdemadara <jrdemadara@protonmail.com>
2024-10-04 21:17:32 +0800

    fix: profile overview

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -55,7 +55,7 @@ const showingNavigationDropdown = ref(false);
                                                 type="button"
                                                 class="capitalize inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                             >
-                                                <span
+                                                <!-- <span
                                                     v-if="
                                                         $page.props.auth.user
                                                             .profile
@@ -65,7 +65,7 @@ const showingNavigationDropdown = ref(false);
                                                         $page.props.auth.user
                                                             .profile.firstname
                                                     }}
-                                                </span>
+                                                </span> -->
                                                 <svg
                                                     class="ms-2 -me-0.5 h-4 w-4"
                                                     xmlns="http://www.w3.org/2000/svg"
@@ -190,11 +190,11 @@ const showingNavigationDropdown = ref(false);
                             <div
                                 class="font-medium text-base text-gray-800 dark:text-gray-200 capitalize"
                             >
-                                <span v-if="$page.props.auth.user.profile">
+                                <!-- <span v-if="$page.props.auth.user.profile">
                                     {{
                                         $page.props.auth.user.profile.firstname
                                     }}
-                                </span>
+                                </span> -->
                             </div>
                             <div
                                 class="font-medium text-sm text-gray-500 capitalize"
