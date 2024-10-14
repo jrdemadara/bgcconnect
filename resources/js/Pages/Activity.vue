882c5d5461773e597a35dfe94168623eca2b77f1
jrdemadara <jrdemadara@protonmail.com>
2024-10-14 14:13:35 +0800

    fix: padding on activity scanning

--- resources/js/Pages/Activity.vue
+++ resources/js/Pages/Activity.vue
@@ -180,7 +180,7 @@ onMounted(() => {
         </template>
 
         <div
-            class="flex flex-col justify-center items-center max-w-7xl mx-auto py-4 sm:px-6 lg:px-8 space-y-10 mt-0 sm:mt-8 bg-white"
+            class="flex flex-col justify-center items-center max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 space-y-10 mt-0 sm:mt-8 bg-white"
         >
             <div class="flex flex-col justify-center items-center w-full px-4">
                 <p class="font-bold text-lg my-4">
