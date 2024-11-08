92f7c23132ba947109ec53e58a1ad513fba49ec9
jrdemadara <jrdemadara@protonmail.com>
2024-11-08 15:44:40 +0800

    fix: prevent double tap on phone verification

--- resources/js/Pages/Profile/VerifyPhone.vue
+++ resources/js/Pages/Profile/VerifyPhone.vue
@@ -10,6 +10,7 @@ import axios from "axios";
 import { toast } from "vue3-toastify";
 
 const timer = ref(0);
+const loading = ref(false);
 let interval: ReturnType<typeof setInterval> | null = null;
 
 const verification_code = ref("");
@@ -39,6 +40,7 @@ const send = async () => {
 };
 
 const verify = () => {
+    loading.value = true;
     axios
         .get("/verify/phone", {
             params: {
@@ -53,6 +55,7 @@ const verify = () => {
             }
         })
         .catch(function (error) {
+            loading.value = false;
             if (error.response && error.response.status == 400) {
                 verification_code_error.value = "Invalid Verification Code";
                 toast.error("Verification Code is invalid.");
@@ -128,6 +131,7 @@ onMounted(() => {
             <div class="flex flex-col space-y-4 w-full">
                 <button
                     @click="verify"
+                    :disabled="loading"
                     class="inline-flex justify-center items-center h-10 px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                 >
                     Verify
